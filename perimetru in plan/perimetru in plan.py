e07f06a6326defecd364e1b3656a10a6aa0a820a
andrei-radu <64258037+andrei-radu@users.noreply.github.com>
2020-09-01 17:31:39 +0300

    Update perimetru in plan.py

--- perimetru in plan/perimetru in plan.py
+++ perimetru in plan/perimetru in plan.py
@@ -7,22 +7,22 @@ for i in range(N):
 d=int(input())
 
 #prelucrare
-Linii=[0 for i in range(D*(D+1))]
-Coloane=[0 for i in range(D*(D+1))]
+Linii=[0 for i in range(D*(D+1))] #creem un vector pentru linii
+Coloane=[0 for i in range(D*(D+1))] #si unul pentru coloane
 for patrat in Patratele:
-    x=int(patrat[0])-1
+    x=int(patrat[0])-1 #luam coordonatele de inceput ale patratului
     y=int(patrat[1])-1
-    Coloane[x*D+y]+=1
+    Coloane[x*D+y]+=1 #salvam liniile si coloanele folosite pentru fiecare patrat
     Coloane[(x+1)*D+y]+=1
     Linii[y*D+x]+=1
     Linii[(y+1)*D+x]+=1
     
-cnt=0
-for lin in Linii:
-    if lin==1:
+cnt=0 #contor pentru perimetru
+for lin in Linii: #numaram cate linii sunt folosite o singura data
+    if lin==1:    #adica reprezinta marginea 
         cnt+=1
         
-for col in Coloane:
+for col in Coloane: #asemenea pt coloane
     if col==1:
         cnt+=1
 
